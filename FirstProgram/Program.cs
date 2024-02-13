6d7fe7e65d75b3c67c7d06dc272207960e513f4a
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-02-13 20:12:09 -0300

    testes fundamentos

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+Pessoa pessoa = new Pessoa("Vinícius", 17);
+pessoa.Apresentar();
