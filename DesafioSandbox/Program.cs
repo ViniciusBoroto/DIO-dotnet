bd7995fd7e653584c23ad72044d4f5c8627a9f75
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-02-17 17:43:23 -0300

    explorando finalizado

---
+++ DesafioSandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MinhaClasse
+{
+    static void Main(string[] args)
+    {
+        int.TryParse(Console.ReadLine(), out int timeInSeconds);
+        // int timeInSeconds = 556;
+        //TODO: Implementar a formula para calcular as horas.
+        var hours = (int)Math.Floor(timeInSeconds / 3600.00);
+        timeInSeconds = timeInSeconds % 3600;
+        //TODO: Implementar a formula para calcular os minutos.
+        var minutes = (int)Math.Floor(timeInSeconds / 60.00);
+        var seconds = timeInSeconds % 60;
+
+        Console.WriteLine($"{hours}:{minutes}:{seconds}");
+
+    }
+}
