495820a22f17e375a4e39ada7bed7c2078e871e9
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-02-24 01:20:31 -0300

    To perdido amanha vejo direito

---
+++ ModuloAPI/Properties/MinhaPrimeiraAPI/Context/AgendaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaPrimeiraAPI.Entities;
+
+namespace MinhaPrimeiraAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) { }
+        public DbSet<Contato> Contato { get; set; }
+    }
+}
--- ModuloAPI/Properties/MinhaPrimeiraAPI/Program.cs
+++ ModuloAPI/Properties/MinhaPrimeiraAPI/Program.cs
@@ -1,3 +1,9 @@
+using MinhaPrimeiraAPI.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MinhaPrimeiraAPI
 {
@@ -8,6 +14,8 @@ namespace MinhaPrimeiraAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AgendaContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
